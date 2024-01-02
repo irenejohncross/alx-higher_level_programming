4806cc93a1a3c8fe6558bdabced21e32c861353a
root <irene.johncross@gmail.com>
2024-01-02 14:52:23 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 99):
-    print('{:02d}, '.format(num), end='')
-print('99')
+    if i == 99:
+        print("{}".format (i))
+else:
+    print("{:02d}, ".format(i), end='')
